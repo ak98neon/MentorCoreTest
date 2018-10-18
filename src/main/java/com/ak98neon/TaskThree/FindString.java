8320f035bf99656c3be8037d148c028883b311b4
artemkudrya <ak98nneon@gmail.com>
2018-10-18 19:31:02 +0300

    Clear Project Three

--- src/main/java/com/ak98neon/TaskThree/FindString.java
+++ src/main/java/com/ak98neon/TaskThree/FindString.java
@@ -1,17 +1,7 @@
 package com.ak98neon.TaskThree;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 public class FindString {
     public static void main(String[] args) {
-        Set<Integer> set = new TreeSet<>();
-        for (int i = 0; i < 10; i++) {
-            set.add(i * (int)(Math.random() * 10));
-        }
 
-        for (Integer i : set) {
-            System.out.print(i + " ");
-        }
     }
 }
